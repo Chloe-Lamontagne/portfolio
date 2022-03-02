f2b7d1c1af619f5bbca63246aa15e676dc757f40
Chloe-Lamontagne <chloemasso9@gmail.com>
2022-03-02 13:21:21 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,7 +107,7 @@ if (document.querySelector(".nav_index")){
 
 						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
 
-						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
+						$bg.css('transform', 'matrix(1,0,0,1,0,' + pos + ')');
 
 					});
 
